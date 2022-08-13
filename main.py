ebe32d51252fd6afcd5e5a996b35c11f70395d0b
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 00:39:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from pyrogram import Client, filters
 async def genStr(bt, message):
   
   api = await bot.ask(message.chat.id, "How are you bruh?")
+  print(api)
+  print(' ')
+  print(api.text)
+  print(' ')
+  print(api.document)
   await message.reply_document(api.document)
   
 if __name__ == "__main__":
