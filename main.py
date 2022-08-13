e8119e36aefe595f5d2e8ba8057f102166a210a4
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-08-13 23:54:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from bot import bot
+from pyromod import listen
+from pyrogram import Client, filters
+
+@bot.on_message(filters.private & filters.command("start"))
+async def genStr(bt, msg):
+  
+  hemlo = await message.reply('Hello')
+  api = await bot.ask(message.chat.id, "How are you bruh?")
+  await hemlo.edit(api.text)
+  
+if __name__ == "__main__":
+    bot.run()
