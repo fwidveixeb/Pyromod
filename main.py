78da4eb9ccb7c42eb134255671f9d870c49a31bf
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 00:48:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from bot import bot
 from pyromod import listen
 from pyrogram import Client, filters
 
-@bot.on_message(filters.private & filters.command("start"))
+@bot.on_message(filters.private & filters.document)
 async def genStr(bt, message):
   
-  one = await bot.ask(message.chat.id, "How are you bruh?")
+  one = await bt.get_messages(message)
   await message.reply_cached_media(one.document.file_id)
   
-  two = await bot.ask(message.chat.id, "How are you bruh?")
+  two = await bot.ask(message.chat.id, "Now send me second document.")
   await message.reply_cached_media(two.document.file_id)
   
 if __name__ == "__main__":
