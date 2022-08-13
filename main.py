7e942a3d8a10282d38e68dde4122dbb4a1b3dce1
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 00:46:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from pyrogram import Client, filters
 @bot.on_message(filters.private & filters.command("start"))
 async def genStr(bt, message):
   
-  api = await bot.ask(message.chat.id, "How are you bruh?")
-  await message.reply_cached_media(api.document.file_id)
+  one = await bot.ask(message.chat.id, "How are you bruh?")
+  await message.reply_cached_media(one.document.file_id)
+  
+  two = await bot.ask(message.chat.id, "How are you bruh?")
+  await message.reply_cached_media(two.document.file_id)
   
 if __name__ == "__main__":
     bot.run()
