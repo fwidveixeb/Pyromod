26b1798a25c6f58acff44ebd6f05b7a83fee74a7
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 16:15:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ from pyrogram import Client
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+SESSION = os.environ.get("SESSION")
 
 
 bot = Client(":memory:",
       api_id=API_ID,
       api_hash=API_HASH,
-      bot_token=BOT_TOKEN)
+      session_string=SESSION)
